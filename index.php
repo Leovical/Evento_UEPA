ececdbb5eac7d1ee64d8f110b3b57685b4438b28
Leonan <pedroleonan85@gmail.com>
2024-10-23 02:16:00 -0300

    Correções links/php

--- index.php
+++ index.php
@@ -1,18 +1,16 @@
 <?php
 
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
 $host = 'localhost';
-$db   = 'evento'; // Verifique se o nome do banco de dados está correto
-$user = 'root'; // Usuário padrão do XAMPP
-$pass = ''; // Se você não configurou uma senha, deixe como uma string vazia
+$db   = 'evento';
+$user = 'root';
+$pass = '';
 $charset = 'utf8mb4';
 
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset"; // Monta a string DSN
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 
 phpinfo();
 try {
-    // Cria a conexão PDO
+
     $pdo = new PDO($dsn, $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     
@@ -25,7 +23,6 @@ try {
         $position = $_POST['position'] ?? '';
         $additionalInfo = $_POST['additional-info'] ?? '';
 
-        // Validação dos campos obrigatórios
         if (!empty($fullName) && !empty($email) && !empty($phone) && !empty($cpf) && !empty($position)) {
             $stmt = $pdo->prepare("INSERT INTO participantes (nome, email, telefone, cpf, cargo, info_adicionais) VALUES (?, ?, ?, ?, ?, ?)");
             $stmt->execute([$fullName, $email, $phone, $cpf, $position, $additionalInfo]);
